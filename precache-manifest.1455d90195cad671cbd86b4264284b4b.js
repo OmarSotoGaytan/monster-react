4a5d783968bc0f935eb91a535ceebd5f30a717c2
Omar Soto <omar.soga97@gmail.com>
2019-08-06 19:52:06 -0500

    Updates

---
+++ precache-manifest.1455d90195cad671cbd86b4264284b4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d27ac49aec8a7e80c4c99d03890948e",
+    "url": "/react-monster/index.html"
+  },
+  {
+    "revision": "ef751f1674e14ce995ce",
+    "url": "/react-monster/static/css/main.093f40de.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-monster/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ef751f1674e14ce995ce",
+    "url": "/react-monster/static/js/main.6db5b769.chunk.js"
+  },
+  {
+    "revision": "74dcad2003ab2836f3f0",
+    "url": "/react-monster/static/js/runtime~main.fb1ed67a.js"
+  }
+]);
